f4afb15520ff69a5778bec25917765cef480f2f6
nyrta1 <nurtai_2005@mail.ru>
2024-03-30 22:14:42 +0500

    feat: created user entity and role entities, added docker compose support

---
+++ backend/src/main/java/com/chatme/chatmeapp/models/entity/Role.java
@@ -0,0 +1,27 @@
+package com.chatme.chatmeapp.models.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity(name = "roles")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @NotNull
+    @ManyToMany(mappedBy = "roles")
+    private List<UserEntity> users = new ArrayList<>();
+}
